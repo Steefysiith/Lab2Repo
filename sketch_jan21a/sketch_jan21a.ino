d6c96e823f07610c0d9190484534d3bfa7746c1f
Steefysiith <123121787+Steefysiith@users.noreply.github.com>
2026-01-21 14:48:11 -0600

    Create sketch_jan21a.ino

---
+++ sketch_jan21a/sketch_jan21a.ino
@@ -0,0 +1,15 @@
+int timestoblink = 1;  // make it global so both functions can see it
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  timestoblink = timestoblink + 3 * 4;  // now timestoblink = 13
+}
+
+void loop() {
+  if (timestoblink > 4) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+  }
+}
